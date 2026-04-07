3e75a57676948b84e802c1ca898414345382acb0
FredericBabon <101555263+FredericBabon@users.noreply.github.com>
2026-04-07 18:16:15 +0200

    18h16

--- libs/faudio/src/FAudio_internal.h
+++ libs/faudio/src/FAudio_internal.h
@@ -684,11 +684,11 @@ void FAudio_INTERNAL_debug_fmt(
 #define LOG_INFO(engine, fmt, ...) PRINT_DEBUG(engine, INFO, "INFO", fmt, __VA_ARGS__)
 #define LOG_DETAIL(engine, fmt, ...) PRINT_DEBUG(engine, DETAIL, "DETAIL", fmt, __VA_ARGS__)
 #define LOG_API_ENTER(engine) do { \
-	PRINT_DEBUG(engine, API_CALLS, "API Enter", "%s", __func__); \
+	PRINT_DEBUG(engine, API_CALLS, "API Enter", "%s", __func__) \
 	FAudio_Log_API_Call(__func__, "ENTER"); \
 } while(0)
 #define LOG_API_EXIT(engine) do { \
-	PRINT_DEBUG(engine, API_CALLS, "API Exit", "%s", __func__); \
+	PRINT_DEBUG(engine, API_CALLS, "API Exit", "%s", __func__) \
 	FAudio_Log_API_Call(__func__, "EXIT"); \
 } while(0)
 #define LOG_FUNC_ENTER(engine) PRINT_DEBUG(engine, FUNC_CALLS, "FUNC Enter", "%s", __func__)
